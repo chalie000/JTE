2141892038ef764771ddeab5cadfc2a97a517426
Lee Chul <winfe79@hanmail.net>
2018-06-07 19:04:39 +0900

    20180607

---
+++ PhoKit/CollectionViewController.swift
@@ -0,0 +1,44 @@
+//
+//  CollectionViewController.swift
+//  PhoKit
+//
+//  Created by formathead on 2018. 6. 5..
+//  Copyright © 2018년 formathead. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViwController:UICollectionViewController
+{
+    
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int
+    {
+        return 1
+        
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+    {
+        return 7
+        
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
+    {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier:"ImageCell", for: indexPath) as! ImageCellController
+        let image = UIImage(named: "images/0\(indexPath.row + 1).jpeg")
+        cell.image.image = image
+        
+        return cell
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ PhoKit/ImageCellController.swift
@@ -0,0 +1,15 @@
+//
+//  ImageCellController.swift
+//  PhoKit
+//
+//  Created by formathead on 2018. 6. 5..
+//  Copyright © 2018년 formathead. All rights reserved.
+//
+
+import UIKit
+
+class ImageCellController:UICollectionViewCell
+{
+    
+    @IBOutlet weak var image: UIImageView!
+}
